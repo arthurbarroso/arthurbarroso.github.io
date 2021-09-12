c843ebc56c8c5b182991864b51c20a5781a36d05
arthurbarroso <arthurdsb_@hotmail.com>
2021-09-12 16:02:04 -0300

    first post

---
+++ generate.clj
@@ -0,0 +1,39 @@
+#!/usr/bin/env bb
+(ns generate
+  (:require [babashka.pods :as pods]
+            [clojure.java.io :as io]
+            [clojure.string :refer [replace]]))
+
+(pods/load-pod "bootleg")
+(require '[pod.retrogradeorbit.bootleg.markdown :as markdown])
+
+(let [posts-dir "./posts"
+      dir (io/file posts-dir)
+      quickstart (slurp "quickstart.html")
+      index-quickstart (slurp "index-quickstart.html")
+      files (.listFiles dir)]
+  (doseq [file files]
+    (let [title (-> (.getName file)
+                    (replace ".md" "")
+                    (replace "-" " "))]
+      (spit (replace (str "./generated/" (.getName file)) ".md" ".html")
+            (->
+              (replace quickstart "{{& body }}"
+                       (markdown/markdown (.getPath file) :html))
+              (replace "{{& title }}" title))))
+    (spit "./generated/index.html"
+          (replace index-quickstart "{{& posts }}"
+                   (->> files
+                        (map (fn [file]
+                               (str "<li>"
+                                    "<a href="
+                                    (replace (.getName file)
+                                             ".md"
+                                             ".html")
+                                    ">"
+                                    (-> (.getName file)
+                                        (replace ".md" "")
+                                        (replace "-" " "))
+                                    "</a>"
+                                    "</li>")))
+                        (clojure.string/join))))))
