69c1dd1fa662bac5a865413d59e66c0538c6f58f
Arthur <arthurdsb_@hotmail.com>
2021-11-29 17:27:14 -0300

    chore(dev): make server work with posts, css and js

--- src/server.clj
+++ src/server.clj
@@ -2,58 +2,73 @@
   "Utilies for development mode"
   (:require [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
-            [hawk.core :as hawk])
-  (:import java.io.File))
+            [core :as core]))
 
-(defn page->route [page-name]
-  [(clojure.string/replace (str "/" page-name) ".html" "")
-   {:get {:handler (fn [_req]
-                     {:body
-                      (slurp (str "./docs/" page-name))})}}])
+(defn asset-route []
+  [["/css/screen.css"
+    {:get {:handler (fn [_req]
+                      {:body
+                       (slurp "./docs/css/screen.css")})}}]
 
-(defn filter-page [page]
-  (not (.isDirectory page)))
+   ["/js/clojure_highlighter.js"
+    {:get {:handler (fn [_req]
+                      {:body
+                       (slurp "./docs/js/clojure_highlighter.js")})}}]])
 
-(defn get-pages []
-  (let [dir (File. "./docs/")
-        files (.listFiles dir)]
-    (->> files
-         (filter filter-page)
-         (map #(.getName %)))))
+(defn post->page [post]
+  [[(str "/" (:slug post))
+    {:get {:handler (fn [_req]
+                      {:body (slurp (str "./docs/"
+                                         (:slug post)
+                                         ".html"))})}}]
+   [(str "/" (:slug post) ".html")
+    {:get {:handler (fn [_req]
+                      {:body (slurp (str "./docs/"
+                                         (:slug post)
+                                         ".html"))})}}]])
 
-(defn create-page-routes [page-names]
-  [""
-   (->> page-names
-        (map page->route))])
+(defn get-posts-pages []
+  (->> (core/get-posts)
+       (map #(dissoc % :raw-content))
+       (map #(dissoc % :preview))
+       (map #(dissoc % :description))
+       (map post->page)))
+
+(defn get-index-page []
+  ["/"
+   {:get
+    {:handler
+     (fn [_req]
+       {:body (slurp "./docs/index.html")})}}])
 
 (defn serve []
   (ring/ring-handler
    (ring/router
     [""
-     ["/css/*" (ring/create-resource-handler {:root "/css"})]
-     (create-page-routes
-      (get-pages))])
+     (get-posts-pages)
+     (get-index-page)
+     (asset-route)])
    (ring/create-default-handler)))
 
 (def server (atom nil))
 
 (defn start! []
-    (reset! server
-        (jetty/run-jetty (serve) {:port 4000 :join? false})))
+  (reset! server
+          (jetty/run-jetty (serve) {:port 4000 :join? false})))
 
 (defn stop! []
   (reset! server
-         (.stop @server)))
+          (.stop @server)))
 
-(defn- auto-reset-handler [ctx _event]
-  (binding [*ns* *ns*]
-    (stop!)
-    (start!)
-    ctx))
+;; (defn- auto-reset-handler [ctx _event]
+;;   (binding [*ns* *ns*]
+;;     (stop!)
+;;     (start!)
+;;     ctx))
 
-(defn watch! []
-  (hawk/watch! [{:paths ["posts"]
-                 :handler auto-reset-handler}]))
+;; (defn watch! []
+;;   (hawk/watch! [{:paths ["posts"]
+;;                  :handler auto-reset-handler}]))
 
 (comment
   (start!)
