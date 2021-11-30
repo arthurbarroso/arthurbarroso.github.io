53443c36ebbd489ea396985b064903d1839fba90
Arthur <arthurdsb_@hotmail.com>
2021-11-30 19:39:19 -0300

    feat: make templates hiccup clj files

--- src/core.clj
+++ src/core.clj
@@ -2,18 +2,18 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
             [markdown.core :as markdown]
-            [selmer.parser :as selmer]
             [slugger.core :as slug]
-            [tick.core :as t]))
+            [tick.core :as t]
+            [templates.post :as post]
+            [templates.archives :as archives]
+            [templates.home :as home]
+            [templates.404 :as page-404]
+            [templates.page :as page-template]
+            [templates.sitemap :as sitemap-template]))
 
 (defn now []
   (->> (t/zoned-date-time)
        (t/format (t/formatter "yyyy-MM-dd"))))
-
-(def config {:archives-uri "archives"
-             :author "Arthur Barroso"
-             :title "((arthur barroso))"
-             :last-mod (now)})
 
 (defn filter-post [post]
   (string/includes? (.getName post) ".md"))
@@ -42,7 +42,12 @@
     (slug/->slug (-> metadata :translation first))
     nil))
 
-(defn post->selmer [post]
+(defn get-date [date]
+  (if date
+    (-> date first t/date)
+    nil))
+
+(defn post->data [post]
   (let [metadata (-> post :metadata)]
     (merge post
            {:title (-> metadata :title first)
@@ -50,7 +55,7 @@
             :description (-> metadata :description first)
             :tags (-> metadata :tags)
             :content (:parsed-content post)
-            :date (-> metadata :date first t/date)
+            :date (-> metadata :date get-date)
             :url (-> metadata :link first)
             :translation (get-translation metadata)})))
 
@@ -84,7 +89,7 @@
        (map md->map)
        (map post->html)
        (map tag->taglist)
-       (map post->selmer)
+       (map post->data)
        (map post->slug)
        (map post->preview)
        (map post->sitemap-url)))
@@ -97,98 +102,79 @@
        (map md->map)
        (map post->html)
        (map tag->taglist)
-       (map post->selmer)
+       (map post->data)
        (map post->slug)
        (map post->preview)
        (map post->sitemap-url)))
 
-(defn render-post [post]
-  (selmer/cache-off!)
-  (selmer/render-file "post.html"
-                      (merge config
-                             {:post post})))
+(defn spit-post [post post-content]
+  (spit (str "./docs/" (:slug post) ".html")
+        post-content))
 
-(defn save-translation [post html-content]
+(defn render-post [post]
+  (post/build-post post))
+
+(defn spit-translation [post post-content]
   (spit
    (str
     "./docs/translations/"
     (:slug post)
     ".html")
-   html-content))
-
-(defn save-post [post html-content]
-  (spit
-   (str
-    "./docs/"
-    (:slug post)
-    ".html")
-   html-content))
+   post-content))
 
 (defn render-archives [posts]
   (let [post-list (reverse (sort-by :date posts))]
     (spit "./docs/archives.html"
-          (selmer/render-file "archives.html"
-                              (merge config {:posts post-list})))))
+          (archives/archives-page post-list))))
 
 (defn render-home [posts]
   (let [post-list (take 3 (reverse (sort-by :date posts)))]
     (spit "./docs/index.html"
-          (selmer/render-file "home.html"
-                              (merge config
-                                     {:posts post-list})))))
+          (home/home-page post-list))))
 
 (defn render-about []
   (let [raw-content (slurp "./pages/about.md")
-        post (post->html {:raw-content
+        page (post->html {:raw-content
                           raw-content})]
     (spit "./docs/about.html"
-          (selmer/render-file "page.html"
-                              (merge config
-                                     {:page {:title "((arthur barroso))"
-                                             :description "about"
-                                             :content (:parsed-content
-                                                       post)}})))))
+          (page-template/build-page
+           (merge page {:title "((arthur barroso))"
+                        :description "about"
+                        :content (:parsed-content page)})))))
 
 (defn render-collage []
   (let [raw-content (slurp "./pages/collage.md")
-        post (post->html {:raw-content
+        page (post->html {:raw-content
                           raw-content})]
     (spit "./docs/collage.html"
-          (selmer/render-file "page.html"
-                              (merge config
-                                     {:title "((arthur barroso))"
-                                      :archives-uri "archives.html"
-                                      :page {:title "((arthur barroso))"
-                                             :description "collage"
-                                             :content (:parsed-content
-                                                       post)}})))))
+          (page-template/build-page
+           (merge page
+                  {:title "((arthur barroso))"
+                   :description "collage"
+                   :content (:parsed-content page)})))))
 
 (defn render-404 []
-  (let [posts (get-posts)]
-    (spit "./docs/404.html"
-          (selmer/render-file "404.html"
-                              (merge config {:posts posts})))))
+  (spit "./docs/404.html"
+        page-404/not-found-page))
 
 (defn create-sitemap []
   (let [posts (get-posts)]
     (spit "./docs/sitemap.xml"
-          (selmer/render-file "sitemap.xml"
-                              {:posts posts
-                               :last-mod (:last-mod config)}))))
+          (sitemap-template/build-sitemap posts (now)))))
 
-(defn render-all [_]
+(defn render-all [& _args]
   (let [posts (get-posts)]
     (doseq [p posts]
-      (save-post p (render-post p)))
-    (render-archives posts)
-    (render-home posts)
-    (render-404)
-    (render-about)
-    (render-collage)
-    (create-sitemap)
-    (let [translations (get-translations)]
-      (doseq [t translations]
-        (save-translation t (render-post t))))))
+      (spit-post p (render-post p))
+      (render-archives posts)
+      (render-home posts)
+      (render-404)
+      (render-about)
+      (render-collage)
+      (create-sitemap)))
+  (let [translations (get-translations)]
+    (doseq [t translations]
+      (spit-translation t (render-post t)))))
 
 (comment
-  (render-all {}))
+  (render-all))
---
+++ src/templates/archives.clj
@@ -0,0 +1,20 @@
+(ns templates.archives
+  (:require [templates.base :as base]
+            [hiccup.core :as h]))
+
+(defn archives [posts]
+  {:tags '()
+   :content
+   [:div {:id "posts"}
+    [:div {:id "page-header"}
+     [:h2 "archives"]]
+    [:ul
+     (for [post posts]
+       [:li (:date post)
+        [:a {:href (:uri post)}
+         (:title post)]])]]})
+
+(defn archives-page [posts]
+  (h/html
+   (base/base {}
+              (archives posts))))
